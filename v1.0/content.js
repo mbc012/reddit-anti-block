78895dd26d19070cec3674c624b93aa74a8a1f64
TheRealLarry <larry@larrymail.xyz>
2024-02-22 11:41:04 +1100

    updates

--- v1.0/content.js
+++ v1.0/content.js
@@ -1,16 +1,33 @@
+const isReddit = () => {
+    return window.location.hostname.includes('reddit.com');
+}
+
 const removeItems = () => {
-    let elements = document.querySelectorAll('[style]');
-    elements.forEach(el => {
-        let style = el.getAttribute('style');
-        if (style.includes('pointer-events: none') || style.includes('overflow: hidden') || style.includes('filter: blur(4px)')) {
-            el.removeAttribute('style');
-        }
+    console.log("[Reddit-Anti-Block] Sweeping for reddit anti-* blocks")
+
+    // Check if page has reddit.com
+    if (!isReddit()) {
+        return;
+    }
+
+    // Check for all desktop blocking modals and remove
+    var elements = document.querySelectorAll('[bundlename="desktop_rpl_nsfw_blocking_modal"]');
+    elements.forEach(function(element) {
+        element.parentNode.removeChild(element);
     });
 
+    // Cont.
     let xpromoElements = document.querySelectorAll('xpromo-nsfw-blocking-modal-desktop');
     xpromoElements.forEach(el => {
         el.remove();
     });
+
+    // Remove overflow and pointerEvent restriction from body
+    var body = document.querySelector('body.v2');
+    if (body && body.style.pointerEvents === 'none' && body.style.overflow === 'hidden') {
+        body.style = "";
+    }
+
 }
 
 setInterval(removeItems, 2000);
